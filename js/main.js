20bedaea75c232850221b54e1eae9528bec33130
Marcin <kazik1142@gmail.com>
2022-02-28 06:39:57 +0100

    added animation and script on navbar

--- js/main.js
+++ js/main.js
@@ -10,6 +10,22 @@ const handleNav = () => {
 			nav.classList.remove('nav--active')
 		})
 	})
+	handleAnimationItems()
 }
 
+const handleAnimationItems = () => {
+	let delayTime = 0
+
+	links.forEach((item) => {
+		item.classList.toggle('nav-animation-items')
+		item.style.animationDelay = '.' + delayTime + 's'
+		delayTime++
+	})
+}
+
+const deleteAnimation = () => {
+	links.forEach((item) => {
+		item.classList.remove('nav-animation-items')
+	})
+}
 navBtn.addEventListener('click', handleNav)
